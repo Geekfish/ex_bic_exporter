0605337d031fc5623766695edcea5c08ed19fbd5
Eleni Lixourioti <contact@eleni.co>
2025-12-09 11:59:58 +0000

    fix: add missing targets and nif versions

--- lib/bic_exporter/native.ex
+++ lib/bic_exporter/native.ex
@@ -7,7 +7,16 @@ defmodule BicExporter.Native do
     crate: :bic_exporter,
     base_url: "https://github.com/Geekfish/ex_bic_exporter/releases/download/v#{version}",
     force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
-    version: version
+    version: version,
+    targets: [
+      "aarch64-apple-darwin",
+      "aarch64-unknown-linux-gnu",
+      "aarch64-unknown-linux-musl",
+      "x86_64-apple-darwin",
+      "x86_64-unknown-linux-gnu",
+      "x86_64-unknown-linux-musl"
+    ],
+    nif_versions: ["2.17", "2.16"]
 
   # NIF stubs - these are replaced when the NIF is loaded
 
